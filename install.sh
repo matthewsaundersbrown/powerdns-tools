17adfc1ca24f9def3279a42c29e756908b847353
Matthew Saunders Brown <msb@stackaas.com>
2022-03-28 09:53:19 -0700

    added certbot-dns-powerdns plugin install

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+apt install pip
+pip install certbot-dns-powerdns
+
+if [[ ! -f /root/.pdns-credentials.ini ]]; then
+  echo "certbot_dns_powerdns:dns_powerdns_api_url =" > /root/.pdns-credentials.ini
+  echo "certbot_dns_powerdns:dns_powerdns_api_key =" >> /root/.pdns-credentials.ini
+  chmod 640 /root/.pdns-credentials.ini
+  echo "Update /root/.pdns-credentials.ini with your settings (this is to be used with certbot-dns-powerdns)."
+fi
+
 chmod 755 bin/*
 cp bin/* /usr/local/bin/
 
 if [[ ! -f /usr/local/etc/pdns.conf ]]; then
-  chmod 640 etc/pdns.conf
   cp etc/pdns.conf /usr/local/etc/pdns.conf
-  echo "Install complete, but sure to update /usr/local/etc/pdns.conf with your settings."
+  chmod 640 /usr/local/etc/pdns.conf
+  echo "Update /usr/local/etc/pdns.conf with your settings."
 fi
